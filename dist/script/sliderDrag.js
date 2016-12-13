a30c46176c08a8179f1e399e718224c15d724d1f
Fuyuan Wang <zoeblow@gmail.com>
2016-12-13 16:06:39 +0800

    chufa

--- dist/script/sliderDrag.js
+++ dist/script/sliderDrag.js
@@ -12,6 +12,8 @@
 * V 1.0 初次提交 滑动验证组件,感谢 @<傀儡 娃娃> 妹子的辛勤工作。 
 * 2016年12月12日15:17:34
 * V 1.1 增加 reset方法. 
+* 2016年12月13日15:33:14
+* V 1.2 将验证是触发条件改为touchend触发，好处1.防止恶意拖动，2.优化页面相应速度，3.优化内存. 
 */
 
 //滑动验证
@@ -41,22 +43,36 @@ var SliderUnlock =(function (){
         $handler.on("touchstart", function(e){
             if (success) {
                 isMove = false;
-                // console.log(11)
             }else{
                 isMove = true; 
-                // console.log(22)
             };
 
             var e = event || window.event,
                 eStart = e.touches[0].pageX, //起始位置
                 eLeft = $handler.position().left;  //position left距离
             $handler.on("touchmove",function(e){
+                if ($(this).hasClass('isMatched')) {
+                    return false;
+                };
                 if(isMove){
-                    var eOffsetX = event.touches[0].pageX - eStart,
-                        disX = eLeft + eOffsetX;
-                        //可被匹配的误差范围
-                        errorLeft = eWide - 20,
-                        errorRight = eWide + 20;
+                    var eOffsetX = e.touches[0].pageX - eStart,disX = eLeft + eOffsetX;
+                    if(disX <= 0){
+                        disX = 0;
+                    }else if(disX >= maxWidth){
+                        disX = maxWidth;
+                    }
+                    $handler.css("left",disX+"px").addClass("isMove");
+                }
+                
+            });
+
+            $handler.on("touchend",function(e){
+                var e = event || window.event
+                var eOffsetX = e.changedTouches[0].pageX - eStart,
+                        disX = eLeft + eOffsetX,
+                        ppLeft = parseInt($matchArea.css('left'));
+                    errorLeft = ppLeft - 10,
+                    errorRight = ppLeft + 10;
                     if(disX <= 0){
                         disX = 0;
                     }else if(disX >= maxWidth){
@@ -65,31 +81,20 @@ var SliderUnlock =(function (){
                     else if(disX >= errorLeft && disX <= errorRight){ //在可误差范围内
                         disX = eWide;
                         $handler.css("left",disX+"px");
+                        // console.log(disX)
                         isMatch();
-                    }else{
-                        $handler.removeClass("isMatched");
-                        $handler.text("|||");
-                        success = false;
                     }
-                    $handler.css("left",disX+"px").addClass("isMove");
-                }
-                
-            });
-
-            $handler.on("touchend",function(){
-                //isMatch();
             });
 
             function isMatch(){
-                var t = setTimeout(function(){
-                    var tdisX = $handler.position().left;
-                    if(tdisX >= errorLeft && tdisX <= errorRight){
-                        $handler.addClass("isMatched").css("left",eWide+"px");
-                        $(".isMatched").text("ok");
-                        success = true;
-                        callback();
-                    }
-                },400);
+                var tdisX = $handler.position().left;
+                // console.log(tdisX)
+                if(tdisX >= errorLeft && tdisX <= errorRight){
+                    $handler.addClass("isMatched").css("left",eWide+"px");
+                    $(".isMatched").text("ok");
+                    success = true;
+                    callback();
+                }
             }
         });
     
@@ -110,7 +115,6 @@ var SliderUnlock =(function (){
         callback();
         _init(ele)
     };
-
     return {
         Reset:Reset,
         Slider:Slider
